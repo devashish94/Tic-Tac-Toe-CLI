b8b76067547d8cad065f90d0f5456f974fc311a7
devahish94 <roy.devashish94@gmail.com>
2022-11-20 20:09:31 +0530

    1. only one exception is left --> player symbol. rest done 2. finished the Game Board decoration 3. made fundamental changes to the game logic to make the code more readable

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,42 +1,41 @@
 import java.util.Scanner;
 
 public class TicTacToe {
-
-    static void board(char[][] arr) {
+    private static void board(char[][] arr) {
         /* This function prints out the board along with borders and dividers, in other words with decoration */
-        String board = "+---+---+---+\n"
+        String gameBoard = "+---+---+---+\n"
                      + "| " + arr[0][0] + " | " + arr[0][1] + " | " + arr[0][2] + " |\n"
                      + "+---+---+---+\n"
                      + "| " + arr[1][0] + " | " + arr[1][1] + " | " + arr[1][2] + " |\n"
                      + "+---+---+---+\n"
                      + "| " + arr[2][0] + " | " + arr[2][1] + " | " + arr[2][2] + " |\n"
                      + "+---+---+---+\n";
-        System.out.println(board);
+        System.out.println(gameBoard);
     }
 
-    static int gameWin(char[][] arr, int round) {
-        // check for the left diagonal
+    private static int gameCondition(char[][] arr, int round) {
+        /* check for the left diagonal */
         if (arr[0][0] != ' ' && (arr[0][0] == arr[1][1]) && (arr[1][1] == arr[2][2])) {
             return 1;
         }
 
-        // check for the right diagonal
+        /* check for the right diagonal */
         if (arr[0][2] != ' ' && (arr[0][2] == arr[1][1]) && (arr[1][1] == arr[2][0])) {
             return 1;
         }
 
         for (int i = 0; i < arr.length; i++) {
-            // check for the rows
+            /* check for the rows */
             if (arr[i][0] != ' ' && (arr[i][0] == arr[i][1]) && (arr[i][1] == arr[i][2])) {
                 return 1;
             }
-            // check for the columns
+            /* check for the columns */
             if (arr[0][i] != ' ' && (arr[0][i] == arr[1][i]) && (arr[1][i] == arr[2][i])) {
                 return 1;
             }
         }
 
-        // check for TIE
+        /* check for TIE */
         if (round == 8) {
             return 404;
         }
@@ -44,8 +43,36 @@ public class TicTacToe {
         return 0;
     }
 
-    static int playerTurn (int round) {
+    private static int inputLocation() {
+        Scanner read = new Scanner(System.in);
+        int location = 10;
+        String l = read.next();
+        if (l.length() != 1) {
+            return location;
+        } else {
+            location = l.charAt(0) - '0';
+        }
+        return location - 1;
+    }
+
+    private static int playerTurn (int round) {
         return round % 2 + 1;
+    }
+
+    private static boolean validLocation(int location) {
+        return location >= 0 && location <= 8;
+    }
+
+    private static boolean cellFilled(char[][] arr, int location) {
+        return arr[location / 3][location % 3 ] == ' ';
+    }
+
+    private static void printInputStatement(int round) {
+        System.out.print("[Player " + playerTurn(round) + "]: Enter the cell number (1 to 9): ");
+    }
+
+    private static void endBanner() {
+        System.out.println("******************************************** END ********************************************");
     }
 
     public static void main(String[] args) {
@@ -57,76 +84,77 @@ public class TicTacToe {
             }
         }
 
-        // Setting the symbols for both the players
+        /* Decoration */
+        System.out.println("\n**************************************** Tic Tac Toe ****************************************");
+
+        /* Set the symbols for both the players */
         System.out.print("PLAYER 1, Enter your chosen symbol from 'o' and 'x': ");
         char playerOne_value = read.next().charAt(0);
 
+        /* Handle the invalid player symbol exception */
         while (playerOne_value != 'o' && playerOne_value != 'x') {
-            System.out.print("[INVALID-INPUT] Enter valid value: ");
+            System.out.print("[Invalid-Symbol] Enter valid value: ");
             playerOne_value = read.next().charAt(0);
         }
         char playerTwo_value = (playerOne_value == 'o') ? 'x' : 'o';
 
-        // Printing the board once for the first player
+        /* Print the board for the first player */
         board(arr);
 
         /* Game Loop */
         int round = 0;
         while (round < 9) {
-            System.out.println("-----------------------------------------------");
+            System.out.println("-------------------------------------------------------------------------");
             System.out.println("[ROUND : " + round + "]\n");
 
+            /* Alternating between Player 1 and Player 2 */
+            printInputStatement(round);
 
-            // Alternating between Player 1 and Player 2
-            System.out.print("Player " + playerTurn(round) + ": Enter the cell number (1 to 9): ");
+            /* Location input */
+            int location = inputLocation();
 
-            // Location input
-            int location = read.nextInt() - 1;
-            if (location < 0 || location > 8) { // Handling invalid location input
-                while (location < 0 || location > 8) {
-                    System.out.print("[INVALID-INPUT] Enter correct location (1-9): ");
-                    location = read.nextInt() - 1;
-                }
+            /* checks for invalid location OR cell already filled condition */
+            while (!validLocation(location) || !cellFilled(arr, location)) {
+                if (!validLocation(location)) System.out.print("[Invalid-Location] ");
+                else System.out.print("[Cell-Already-Filled] ");
+                printInputStatement(round);
+                location = inputLocation();
             }
 
-            // Checking if input cell is already filled
-            while (arr[location / 3][location % 3 ] != ' ') {
-                System.out.println("[INVALID-MOVE] The entered Location is already filled");
-                System.out.print("Player " + playerTurn(round) + ": Enter the cell number (1 to 9): ");
-                location = read.nextInt() - 1; // Location input
-            }
-
-            // Putting the 'o' or 'x' inside the cell
+            /* Putting the 'o' or 'x' inside the cell */
             arr[location / 3][location % 3] = (round % 2 == 0) ? playerOne_value : playerTwo_value;
 
-            // Print the board with Decoration
+            /* Print the board with Decoration */
             board(arr);
 
-            /* GAME WIN LOGIC WILL BE PLACED HERE */
-            int condition = gameWin(arr, round);
+            /* Store the value of gameCondition method to avoid multiple method calls */
+            int condition = gameCondition(arr, round);
+
+            /* checks for WIN condition */
             if (condition == 1) {
-                System.out.println("Player " + playerTurn(round) + " WINS the GAME !!");
+                System.out.println("!! Player-" + playerTurn(round) + " WINS the Game !!");
+                endBanner();
                 return;
-            }
-            else if (condition == 404) {
-                System.out.println("The Game is TIED");
+            } else if (condition == 404) { /* check for TIE condition */
+                System.out.println("!! The Game is TIED !!");
+                endBanner();
                 return;
             }
 
-            // increment the round number
+            /* increment the round by 1 */
             round++;
         }
     }
 }
 
-/*      => TODO-list <=
+/*      TO-DO List:
  *   -> [IMPORTANT] Create a script or Test that runs all possible test cases and gives Summary
+ *   -> [DIFFICULT TO-DO] Decorate the Board with the WIN condition
+ *   -> [OPTIONAL][PARTIALLY-DONE] Refine Decorations for every round (starting borders, etc)
  *   -> [DONE] Set symbols for both Players
  *   -> [DONE] Board Print with decoration
  *   -> [DONE] Player Location Input
  *   -> [DONE] Handling cell already filled logic
  *   -> [DONE] Game Win logic with TIE
- *   -> [DIFFICULT TO-DO] Decorate the Board with the WIN condition
- *   -> [DONE-KINDA]Exception Handling for invalid data type input
- *   -> [OPTIONAL] Refine Decorations for every round (starting borders, etc)
-*/
+ *   -> [DONE] Exception Handling for invalid data type input
+ */
